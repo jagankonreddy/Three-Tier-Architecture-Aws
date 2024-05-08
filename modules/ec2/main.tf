8856bb1f244b0d3601c109644691525c3381de45
jagankonreddy <reddyjagan689@gmail.com>
2024-05-08 23:49:44 +0530

    added the project files

---
+++ modules/ec2/main.tf
@@ -0,0 +1,16 @@
+
+
+# Module template for the private application/web tier servers
+
+resource "aws_instance" "two_tier_server" {
+    ami = "ami-007020fd9c84e18c7"
+    instance_type = "t2.micro"
+    key_name = "Two_Tier_Key_Pair"
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = var.vpc_security_group_ids
+    user_data = "${file("${path.module}/services.sh")}"
+
+    tags = {
+      Name = var.Name
+    }
+}
